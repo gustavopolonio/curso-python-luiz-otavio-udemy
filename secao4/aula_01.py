a23834e9700774323a60aa4ca49aafa2176f5b34
gustavopolonio <gustavopolonio1@gmail.com>
2024-01-12 16:59:24 -0300

    parameter default value

--- secao4/aula_01.py
+++ secao4/aula_01.py
@@ -1,5 +1,6 @@
 """
   Introdução às funções (def)
+  Parâmetro é o nome da "variável" dentro dos parênteses da fç, argumento é o valor passado para o parâmetro no momento da execução da função.
   Por padrão, funções python retornam None
 """
 
@@ -8,3 +9,18 @@ def imprimir(a, b, c = 10):  # 'a', 'b' e 'c' são parâmetros
 
 imprimir(1, 2, 3)  # '1', '2 e '3' são argumentos
 imprimir(0, 5)
+
+
+"""
+  Como saber ser um parâmetro foi enviado para uma fç ou não:
+  Seto o valor padrão desse parâmetro como 'None' e confiro se ele é None
+"""
+def sum(x, y, z = None):
+  if z is None:
+    print(x + y)
+  else:
+    print(f'O valor de z é: {z}')
+    print(x + y + z)
+
+sum(2, 10)
+sum(2, 10, 0)
