f96c56c5594f3f040a1086496cdbc4ea9b24295a
gustavopolonio <gustavopolonio1@gmail.com>
2024-01-14 16:11:55 -0300

    function scope

--- secao4/aula_01.py
+++ secao4/aula_01.py
@@ -24,3 +24,31 @@ def sum(x, y, z = None):
 
 sum(2, 10)
 sum(2, 10, 0)
+
+
+"""
+  Escopo de funções em Python
+  Escopo significa o local onde aquele código pode atingir.
+  Existe o escopo global e local.
+  O escopo global é o escopo onde todo o código é alcançavel.
+  O escopo local é o escopo onde apenas nomes do mesmo local podem ser alcançados.
+"""
+x = 1
+
+def function_1():
+  # global x  # Estou falando que o x abaixo é o global
+  # x = 2  # Atualizando o valor do x global
+  x = 2
+
+  def function_2():
+    y = 3
+    # global x  # Também atualiza o valor do x global
+    x = 4
+    print(x, y)
+  
+  function_2()
+  print(x)
+
+print(x)
+function_1()
+print(x)
